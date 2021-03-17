b175737a442a6f604aa4ecaca0fccbd6da7f2869
afrogal.dev <afua.mkb@gmail.com>
2021-03-17 19:32:53 +0000

    Login & Register Screens

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBJCFKJz-WJqgmYStB6DPEQ0zCPFwLFeC0",
+    authDomain: "signal-clone-aed43.firebaseapp.com",
+    projectId: "signal-clone-aed43",
+    storageBucket: "signal-clone-aed43.appspot.com",
+    messagingSenderId: "332283259285",
+    appId: "1:332283259285:web:76caa117f5f85b2ab3ee9c",
+    measurementId: "G-30X3Z70Z7Q"
+  };
